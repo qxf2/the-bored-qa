e535d347a8d999aa0e7af46a9f81ac9b2dc14d2d
kirancv4 <kiran.cv@qxf2.com>
2020-03-11 12:06:17 +0530

    reading json file.

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ The Bored QA
 from flask import Flask
 import random
 import csv
-
+import json
+from flask import request
 
 #Importing render_template to include html files and css.
 from flask import render_template
@@ -29,8 +30,13 @@ def get_quotes():
     # Pass the selected quote to the template.
     return render_template('quotes.html',quote=selected_quote)
     
-
-
+@app.route("/read",methods=['POST'])
+def read_json():
+    f = open('situational_questions.json')
+    data = json.load(f)
+    for i in data:
+        print (i)
+        return render_template('quotes.html',quote=i)
 #'app' is the instance for the Flask application.Run the application in a particular port!!
 if __name__=='__main__' :
     app.run(host='0.0.0.0', port=5000, debug= True)
