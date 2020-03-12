da821c453a2521306013c3f089c5dfe25811adc2
PreedhiVivek <preedhi.vivek@qxf2.com>
2020-03-12 12:54:44 +0530

    File contains the open ended questions for the Think About Testing tool.

---
+++ questions.py
@@ -0,0 +1,83 @@
+import json
+
+questions ={
+    "question_1" : [
+    {
+    "type":"open-question",
+    "question":"How do you test the emoticons compatibility across different platforms?",
+    "example-apps": ["Skype", "Slack", "MS Teams"]
+    } ],
+    "question_2" : [
+    {
+    "type":"open-question",
+    "question":"How do I test recording?",
+    "example-apps": ["GoogleHangouts","MS Team"]
+    }],
+    "question_3" : [
+    {
+    "type":"open-question",
+    "question":"How do you test 'block user' functionality in a chat app?",
+    "example-apps": ["Skype", "Slack"]
+    }],
+    "question_4" : [
+    {
+    "type":"open-question",
+    "question":"Does the recording pick up the background noise from one of the participant?",
+    "example-apps": ["GoogleHangouts","MS Team"]
+    }],
+    "question_5" : [
+    {
+    "type":"open-question",
+    "question":"How do you test a video call application temporarily disables video for someone who has high latency?",
+    "example-apps": ["GoogleHangouts","MS Team"]
+    }],
+    "question_6" : [
+    {
+    "type":"open-question",
+    "question":"How does a word processing application hande mulitple people editing the same line at the same time?",
+    "example-apps": ["Google Docs", "MS Office"]
+    }],
+    "question_7" : [
+    {
+    "type":"open-question",
+    "question":"How does a CI/CD tool handle a queue of events that trigger a test?",
+    "example-apps": ["Jenkins","Circle CI"]
+    }],
+    "question_8" : [
+    {
+    "type":"open-question",
+    "question":"How do you check the plugin's compatibility for a platform that changes frequently?",
+    "example-apps": ["Chrome","Firefox","Jenkins"]
+    }],
+    "question_9" : [
+    {
+    "type":"open-question",
+    "question":"How do you test if the search returns the right results on chat platforms?",
+    "example-apps": ["Skype", "Slack", "MS Teams"]
+    }],
+    "question_10" : [
+    {
+    "type":"open-question",
+    "question":"How does the video chat application knows there is latency in a connection?",
+    "example-apps": ["Skype", "Slack", "MS Teams"]
+    }],
+    "question_11" : [
+    {
+    "type":"open-question",
+    "question":"How are the messages ordered in a group chat? messages first sent to the application server are listed first? messages recieved at the server are listed first?",
+    "example-apps": ["Skype", "Slack", "MS Teams"]
+    }],
+    "question_12" : [
+    {
+    "type":"open-question",
+    "question":"How do you test calendar events sent to people across different time zones?",
+    "example-apps": ["Google", "Outlook"]
+    }],
+    "question_13" : [
+    {
+    "type":"open-question",
+    "question":"How do you decide the test coverage with regards to verifying the languages an IDE supports?",
+    "example-apps": ["VS Code","Notepad","SublimeText"]
+    }]
+} 
+
