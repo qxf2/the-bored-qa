318dc5fb0c06b7709894684f7a34d60f1fd9186f
Raji Gali <raji@qxf2.com>
2020-03-12 13:45:57 +0530

    tested

--- architecture_diagrams.py
+++ architecture_diagrams.py
@@ -1,18 +1,73 @@
 """
 create a list of json objects in a python file to store architecture diagrams
-json object format should be {name:"",image_url:"",source:"",type:""}
+json object format should be [{name:"",image_url:"",source:"",type:""}]
 """
 
-diagrams = [{name:"real time monitoring and debugging",image_url:"https://drive.google.com/open?id=1zrjxYfzxXShJL_7rEyQsdu5vfUTR_MLQ",source:"https://bytes.swiggy.com/architecture-and-design-principles-behind-the-swiggys-delivery-partners-app-4db1d87a048a",type:"block"},
-            {name:"celery queue",image_url:"https://drive.google.com/open?id=1XKhlgLisXHwI74BkpE_KgVEpaXovfS_p",source:"https://cloud.google.com/blog/products/data-analytics/scale-your-composer-environment-together-your-business",type:"block"},
-            {name:"real time historical logs",image_url:"https://drive.google.com/open?id=1sXlNSw6L2FBbCoAfmC4tmbAXrrFo63DY",source:"https://bytes.swiggy.com/architecture-and-design-principles-behind-the-swiggys-delivery-partners-app-4db1d87a048a",type:"block"},
-            {name:"poor network scenarios",image_url:"https://drive.google.com/open?id=1RjVPch1nOVSKuq3bstp50ZfTjJJNdxA3",source:"https://bytes.swiggy.com/architecture-and-design-principles-behind-the-swiggys-delivery-partners-app-4db1d87a048a",type:"block"},
-            {name:"aws lamda",image_url:"https://drive.google.com/open?id=1EAN2HP4qaf0iYipaibG4URdIb9FJp3gf",source:"https://aws.amazon.com/blogs/architecture/architecting-a-low-cost-web-content-publishing-system/",type:"block"},
-            {name:"order management",image_url:"https://drive.google.com/open?id=1Lw5NRYvKE6feosrls4P7lr1nlgoyf-_l",source:"https://bytes.swiggy.com/architecture-and-design-principles-behind-the-swiggys-delivery-partners-app-4db1d87a048a",type:"block"},
-            {name:"reddis twitter",image_url:"https://drive.google.com/open?id=1u2eTu1bij3u-3szwfAT4Ubg4Pm5-vd4R",source:"https://medium.com/@narengowda/system-design-for-twitter-e737284afc95",type:"block"},
-            {name:"swiggy data flow",image_url:"https://drive.google.com/open?id=1gNxTg2pFJAYy9AmG-hFqmpxwVbHZ8UfQ",source:"https://bytes.swiggy.com/architecture-and-design-principles-behind-the-swiggys-delivery-partners-app-4db1d87a048a",type:"block"},
-            {name:"swiggy partners",image_url:"https://drive.google.com/open?id=1O8q19or1i778mYvoHPpFqhQN19UD83Ib",source:"https://bytes.swiggy.com/architecture-and-design-principles-behind-the-swiggys-delivery-partners-app-4db1d87a048a",type:"block"},
-            {name:"twitter system design",image_url:"https://drive.google.com/open?id=1PdPV4NIL7ELn1Ojr9NuNecxLCqoX_qfB",source:"https://medium.com/@narengowda/system-design-for-twitter-e737284afc95",type:"block"}]
+diagrams = [
+        {
+            'name':"real time monitoring and debugging",
+            'image_url':"https://drive.google.com/open?id=1zrjxYfzxXShJL_7rEyQsdu5vfUTR_MLQ",
+            'source':"https://bytes.swiggy.com/architecture-and-design-principles-behind-the-swiggys-delivery-partners-app-4db1d87a048a",
+            'type':"block"
+        },
+        {
+            'name':"celery queue",
+            'image_url':"https://drive.google.com/open?id=1XKhlgLisXHwI74BkpE_KgVEpaXovfS_p",
+            source:"https://cloud.google.com/blog/products/data-analytics/scale-your-composer-environment-together-your-business",
+            type:"block"
+        },
+        {
+            name:"real time historical logs",
+            image_url:"https://drive.google.com/open?id=1sXlNSw6L2FBbCoAfmC4tmbAXrrFo63DY",
+            source:"https://bytes.swiggy.com/architecture-and-design-principles-behind-the-swiggys-delivery-partners-app-4db1d87a048a",
+            type:"block"
+        },
+        {
+            name:"poor network scenarios",
+            image_url:"https://drive.google.com/open?id=1RjVPch1nOVSKuq3bstp50ZfTjJJNdxA3",
+            source:"https://bytes.swiggy.com/architecture-and-design-principles-behind-the-swiggys-delivery-partners-app-4db1d87a048a",
+            type:"block"
+        },
+        {
+            name:"aws lamda",
+            image_url:"https://drive.google.com/open?id=1EAN2HP4qaf0iYipaibG4URdIb9FJp3gf",
+            source:"https://aws.amazon.com/blogs/architecture/architecting-a-low-cost-web-content-publishing-system/",
+            type:"block"
+        },
+        {
+            name:"order management",
+            image_url:"https://drive.google.com/open?id=1Lw5NRYvKE6feosrls4P7lr1nlgoyf-_l",
+            source:"https://bytes.swiggy.com/architecture-and-design-principles-behind-the-swiggys-delivery-partners-app-4db1d87a048a",
+            type:"block"
+        },
+        {
+            name:"reddis twitter",
+            image_url:"https://drive.google.com/open?id=1u2eTu1bij3u-3szwfAT4Ubg4Pm5-vd4R",
+            source:"https://medium.com/@narengowda/system-design-for-twitter-e737284afc95",
+            type:"block"
+        },
+        {
+            name:"swiggy data flow",
+            image_url:"https://drive.google.com/open?id=1gNxTg2pFJAYy9AmG-hFqmpxwVbHZ8UfQ",
+            source:"https://bytes.swiggy.com/architecture-and-design-principles-behind-the-swiggys-delivery-partners-app-4db1d87a048a",
+            type:"block"
+        },
+        {
+            name:"swiggy partners",
+            image_url:"https://drive.google.com/open?id=1O8q19or1i778mYvoHPpFqhQN19UD83Ib",
+            source:"https://bytes.swiggy.com/architecture-and-design-principles-behind-the-swiggys-delivery-partners-app-4db1d87a048a",
+            type:"block"
+        },
+        {
+            name:"twitter system design",
+            image_url:"https://drive.google.com/open?id=1PdPV4NIL7ELn1Ojr9NuNecxLCqoX_qfB",
+            source:"https://medium.com/@narengowda/system-design-for-twitter-e737284afc95",
+            type:"block"
+        }
+    ]
 
 
 
+
+if __name__ == "__main__": 
+    print ("Here is the list of the architechture diagrams: %s" %diagrams) 
