45a9869ffc9b22a2e08df80e5001530dcd652f1a
Rohan Joshi <rohan.j@qxf2.com>
2020-03-12 12:01:44 +0530

    common linux command questions in json format

---
+++ explain_me.py
@@ -0,0 +1,83 @@
+
+
+data = [
+    {
+        "type":"explain-me",
+        "question":"pip install --upgrade robotframework-seleniumlibrary",
+        "src":"https://github.com/robotframework/SeleniumLibrary",
+        "answers":['Install Python-specific modules and use --upgrade to upgrade the existing python specific module']
+    },
+    {
+        "type":"explain-me",
+        "question":"sudo pip install -r requirements.txt",
+        "src":"",
+        "answers":['Install pre-requisite libraries/dependencies']
+    },
+    {
+        "type":"explain-me",
+        "question":"git submodule sync",
+        "src":"",
+        "answers":["apply the remote repo's configuration to your local submodule repos. Make the child repo follow the parent repo. It is similar to master/slave"]
+    },
+    {
+        "type":"explain-me",
+        "question":"source path to python exe location/scripts/activate",
+        "src":"",
+        "answers":['Activating python virtualenv']
+    },
+    {
+        "type":"explain-me",
+        "question":"",
+        "src":"https://github.com/M4cs/BabySploit",
+        "answers":[]
+    },
+    {
+        "type":"explain-me",
+        "question":"sudo apt-get update\nsudo apt-get upgrade\nsudo apt-get install",
+        "src":"",
+        "answers":['These are the common commands in Linux to install and upgrade packages.']
+    },
+    {
+        "type":"explain-me",
+        "question":"",
+        "src":"https://github.com/IBM/ibm.github.io",
+        "answers":[]
+    },
+    {
+        "type":"explain-me",
+        "question":"python -m http.server {port}",
+        "src":"",
+        "answers":['To run the tests on the desired port or to test changes locally, From within the top level folder of the cloned repository run']
+    },
+    {
+        "type":"explain-me",
+        "question":"docker-compose up --build",
+        "src":"https://github.com/PokeAPI/pokeapi",
+        "answers":['To start the process in detached mode','You can specify the -d switch to start in detached mode.']
+    },
+    {
+        "type":"explain-me",
+        "question":"",
+        "src":"https://github.com/rust-lang/rustlings",
+        "answers":[]
+    },
+    {
+        "type":"explain-me",
+        "question":"curl -L https://git.io/rustlings | bash",
+        "src":"",
+        "answers":['to install the package to the default path']
+    },
+    {
+        "type":"explain-me",
+        "question":'xvfb-run --server-args="-screen 0 1024x768x24" python test_script.py',
+        "src":"https://github.com/joyzoursky/docker-python-chromedriver",
+        "answers":['To run the script in a headless display screen with the given dimensions.']
+    },
+    {
+        "type":"explain-me",
+        "question":"ssh -i /Users/yaroslav/.ncluster/ncluster5-yaroslav-316880547378-us-east-1.pem -o StrictHostKeyChecking=no ubuntu@18.206.193.26tmux a",
+        "src":"https://github.com/joyzoursky/docker-python-chromedriver",
+        "answers":['Ssh/Login into a ubuntu instance using a .pem file(trust chain with user identification) where host key is not being checked for login purposes.']
+    }
+    
+]
