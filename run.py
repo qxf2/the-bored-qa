56a52dc3e7714ef598ef6245ac5d218fd45a59cc
Qxf2 <mak@qxf2.com>
2020-07-02 14:36:12 +0530

    A fully working MVP of the bored QA application.
    Working copy of index page
    
    Improved app based on pylint suggestions
    
    Version 1.0 of a clean UI for the bored-qa
    
    Added bug type of question too
    
    Wrapped up writing the informational pages on the site

---
+++ run.py
@@ -0,0 +1,5 @@
+"""
+This file starts the bored QA web application
+"""
+from the_bored_qa import app
+app.run(host='0.0.0.0', debug=True)
---
+++ the_bored_qa/static/js/bored_qa.js
@@ -0,0 +1,4 @@
+function toggleInstructions(id) {
+    var div = document.getElementById(id);
+    div.style.display = div.style.display == "none" ? "block" : "none";
+}
