65eb570d0237fda82eea7d0f6c3a1518079ae018
qxf2 <mak@qxf2.com>
2020-07-03 05:39:55 +0530

    Update application.py to suit AWS Beanstalk deploy
    Updated application.py to suit AWS Beanstalk deploy. I am not sure if this works but I decided to remove the host and also rename app to application.

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 """
 This file starts the bored QA web application
 """
-from the_bored_qa import app
-app.run(host='0.0.0.0', debug=True)
+from the_bored_qa import app as application
+application.run()
