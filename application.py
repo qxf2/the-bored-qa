4bb161c47a65430a2eba0d2da699a1f5f1ff134a
Qxf2 <mak@qxf2.com>
2020-07-03 06:55:10 +0530

    Tried putting the app within a __main__

--- application.py
+++ application.py
@@ -2,4 +2,7 @@
 This file starts the bored QA web application
 """
 from the_bored_qa import app as application
-application.run()
+
+#----START OF SCRIPT
+if __name__ == "__main__":
+    application.run()
